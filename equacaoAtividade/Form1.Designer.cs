81e8b7e08a71784bf6379214c28e3a04a2affc11
gFranca <gabrielfranca27@hotmail.com>
2021-03-31 19:52:33 -0300

    Add project files.

---
+++ equacaoAtividade/Form1.Designer.cs
@@ -0,0 +1,261 @@
+﻿
+namespace equacaoAtividade
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtA = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtB = new System.Windows.Forms.TextBox();
+            this.txtC = new System.Windows.Forms.TextBox();
+            this.txtDelta = new System.Windows.Forms.TextBox();
+            this.txtX1 = new System.Windows.Forms.TextBox();
+            this.txtX2 = new System.Windows.Forms.TextBox();
+            this.lblMensagem = new System.Windows.Forms.Label();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtA
+            // 
+            this.txtA.Location = new System.Drawing.Point(141, 48);
+            this.txtA.Name = "txtA";
+            this.txtA.Size = new System.Drawing.Size(55, 20);
+            this.txtA.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.label1.Location = new System.Drawing.Point(100, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(20, 19);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "A";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.label2.Location = new System.Drawing.Point(100, 103);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(18, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "B";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.label3.Location = new System.Drawing.Point(100, 162);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(18, 20);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "C";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.label4.Location = new System.Drawing.Point(364, 51);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(48, 19);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Delta";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.label5.Location = new System.Drawing.Point(364, 166);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(25, 19);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "X²";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.label6.Location = new System.Drawing.Point(364, 110);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(25, 19);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "X¹";
+            // 
+            // txtB
+            // 
+            this.txtB.Location = new System.Drawing.Point(141, 105);
+            this.txtB.Name = "txtB";
+            this.txtB.Size = new System.Drawing.Size(55, 20);
+            this.txtB.TabIndex = 11;
+            // 
+            // txtC
+            // 
+            this.txtC.Location = new System.Drawing.Point(141, 163);
+            this.txtC.Name = "txtC";
+            this.txtC.Size = new System.Drawing.Size(55, 20);
+            this.txtC.TabIndex = 12;
+            // 
+            // txtDelta
+            // 
+            this.txtDelta.Enabled = false;
+            this.txtDelta.Location = new System.Drawing.Point(426, 48);
+            this.txtDelta.Name = "txtDelta";
+            this.txtDelta.Size = new System.Drawing.Size(55, 20);
+            this.txtDelta.TabIndex = 13;
+            // 
+            // txtX1
+            // 
+            this.txtX1.Enabled = false;
+            this.txtX1.Location = new System.Drawing.Point(426, 107);
+            this.txtX1.Name = "txtX1";
+            this.txtX1.Size = new System.Drawing.Size(55, 20);
+            this.txtX1.TabIndex = 14;
+            // 
+            // txtX2
+            // 
+            this.txtX2.Enabled = false;
+            this.txtX2.Location = new System.Drawing.Point(426, 163);
+            this.txtX2.Name = "txtX2";
+            this.txtX2.Size = new System.Drawing.Size(55, 20);
+            this.txtX2.TabIndex = 15;
+            // 
+            // lblMensagem
+            // 
+            this.lblMensagem.AutoSize = true;
+            this.lblMensagem.BackColor = System.Drawing.Color.Transparent;
+            this.lblMensagem.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.lblMensagem.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMensagem.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.lblMensagem.Location = new System.Drawing.Point(0, 428);
+            this.lblMensagem.Name = "lblMensagem";
+            this.lblMensagem.Size = new System.Drawing.Size(0, 22);
+            this.lblMensagem.TabIndex = 16;
+            this.lblMensagem.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btnCalcular.FlatAppearance.BorderSize = 0;
+            this.btnCalcular.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCalcular.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalcular.ForeColor = System.Drawing.Color.White;
+            this.btnCalcular.Location = new System.Drawing.Point(159, 269);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(84, 34);
+            this.btnCalcular.TabIndex = 17;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseMnemonic = false;
+            this.btnCalcular.UseVisualStyleBackColor = false;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click_1);
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btnLimpar.FlatAppearance.BorderSize = 0;
+            this.btnLimpar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLimpar.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimpar.ForeColor = System.Drawing.Color.Transparent;
+            this.btnLimpar.Location = new System.Drawing.Point(367, 269);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(84, 34);
+            this.btnLimpar.TabIndex = 18;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = false;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(638, 450);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.lblMensagem);
+            this.Controls.Add(this.txtX2);
+            this.Controls.Add(this.txtX1);
+            this.Controls.Add(this.txtDelta);
+            this.Controls.Add(this.txtC);
+            this.Controls.Add(this.txtB);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtA);
+            this.MaximumSize = new System.Drawing.Size(654, 489);
+            this.MinimumSize = new System.Drawing.Size(654, 489);
+            this.Name = "Form1";
+            this.Text = "Calculadora - Equação de Segundo Grau";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtA;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtB;
+        private System.Windows.Forms.TextBox txtC;
+        private System.Windows.Forms.TextBox txtDelta;
+        private System.Windows.Forms.TextBox txtX1;
+        private System.Windows.Forms.TextBox txtX2;
+        private System.Windows.Forms.Label lblMensagem;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Button btnLimpar;
+    }
+}
+
---
+++ equacaoAtividade/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace equacaoAtividade
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void btnCalcular_Click_1(object sender, EventArgs e)
+        {
+            //Verificação dos Dados
+
+            if (txtA.Text == "" && txtB.Text == "" && txtC.Text == "")
+            {
+                lblMensagem.Text = "Todos os campos estão vazios";
+            }
+
+            else if (txtA.Text == "" && txtB.Text == "")
+            {
+                lblMensagem.Text = "Os campos A e B estão vazios!";
+            }
+
+            else if (txtB.Text == "" && txtC.Text == "")
+            {
+                lblMensagem.Text = "Os campos B e C estão vazios!";
+            }
+
+            else if (txtA.Text == "" && txtC.Text == "")
+            {
+                lblMensagem.Text = "Os campos A e C estão vazios!";
+            }
+
+
+
+            else if (txtA.Text.Length <= 0 && txtB.Text.Length > 0 && txtC.Text.Length > 0)
+            {
+                lblMensagem.Text = "Esta é uma equação de primeiro grau!";
+            }
+
+            else if (txtA.Text == "")
+            {
+                lblMensagem.Text = "O campo A está vazio!";
+            }
+
+            else if (txtB.Text == "")
+            {
+                lblMensagem.Text = "O campo B está vazio!";
+            }
+
+            else if (txtC.Text == "")
+            {
+                lblMensagem.Text = "O campo C está vazio!";
+            }
+
+            else // Realização da conta
+            {
+                //A, B e C
+                float A = float.Parse(txtA.Text);
+                float B = float.Parse(txtB.Text);
+                float C = float.Parse(txtC.Text);
+
+
+                //Delta
+                float delta = (float)(Math.Pow(2, B) - 4 * A * C);
+
+                if (delta < 0)
+                {
+                    lblMensagem.Text = "Não foi possível calcular as raízes porque o Delta é negativo";
+
+                }
+
+                else if (delta == 0)
+                {
+                    float baskara = -B / (2 * A);
+
+                    txtX1.Text = Convert.ToString(baskara);
+                    txtDelta.Text = Convert.ToString(delta);
+
+                    lblMensagem.Text = "Cálculo concluído!";
+                }
+                else
+                {
+                    //Resultado
+                    float baskara1 = (float)(-B + Math.Sqrt(delta)) / (2 * A);
+                    float baskara2 = (float)(-B - Math.Sqrt(delta)) / (2 * A);
+
+                    txtDelta.Text = Convert.ToString(delta);
+                    txtX1.Text = Convert.ToString(Math.Round(baskara1, 2));
+                    txtX2.Text = Convert.ToString(Math.Round(baskara2, 2));
+
+                    lblMensagem.Text = "Cálculo concluído!";
+                }
+            }
+        }
+
+        private void btnLimpar_Click_1(object sender, EventArgs e)
+        {
+            //A, B e C
+            txtA.Text = "";
+            txtB.Text = "";
+            txtC.Text = "";
+
+            //Delta
+            txtDelta.Text = "";
+
+            //X¹
+            txtX1.Text = "";
+
+            //X²
+            txtX2.Text = "";
+
+            txtA.Focus();
+
+            lblMensagem.Text = "Limpeza concluída!";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
